ba122949b806b74f30412f279d2ede3d180a669d
Shea <7898259+cmd-line@users.noreply.github.com>
2021-05-24 01:08:47 -0500

    funny

--- panda/board/safety/safety_gm_ascm.h
+++ panda/board/safety/safety_gm_ascm.h
@@ -35,9 +35,9 @@ static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 }
 
 const safety_hooks gm_ascm_hooks = {
-  .init = nooutput_init,
+  .init = alloutput_init,
   .rx = default_rx_hook,
   .tx = alloutput_tx_hook,
-  .tx_lin = nooutput_tx_lin_hook,
+  .tx_lin = alloutput_tx_lin_hook,
   .fwd = gm_ascm_fwd_hook,
 };
